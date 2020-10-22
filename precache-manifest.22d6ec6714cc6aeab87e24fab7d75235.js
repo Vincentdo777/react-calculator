486a5c47f9ea766af855f0780728d1782660fff0
fatvoltage777 <dominhhoangvincent@gmail.com>
2020-10-22 20:57:04 +1100

    Updates

---
+++ precache-manifest.22d6ec6714cc6aeab87e24fab7d75235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4cb884007d26c729a946c9c2efcfd98",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "334a91e4b38f6f52d790",
+    "url": "/react-calculator/static/css/main.73b54c44.chunk.css"
+  },
+  {
+    "revision": "8cbdd6cad82e8bff6536",
+    "url": "/react-calculator/static/js/2.c4f81bb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.c4f81bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334a91e4b38f6f52d790",
+    "url": "/react-calculator/static/js/main.7c76924d.chunk.js"
+  },
+  {
+    "revision": "d691a0029daf76af4c74",
+    "url": "/react-calculator/static/js/runtime-main.ff3837be.js"
+  }
+]);
